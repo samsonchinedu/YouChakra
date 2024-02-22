d43f7da880223810861af1f0f30d1c0e7b0449a0
samsonchinedu <udenichisamson@gmail.com>
2024-02-22 12:21:39 +0100

    remove theme.ts file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-// app/page.tsx
 'use client'
+
 import { Link } from '@chakra-ui/next-js'
 
 export default function Page() {
   return (
-    <Link href='/about' color='blue.400' _hover={{ color: 'blue.500' }}>
+    <Link href='./pages/about' color='green.400' _hover={{ color: 'green.500' }}>
       About
     </Link>
   )
--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -1,15 +1,18 @@
+'use client'
+
+import { Link } from '@chakra-ui/next-js'
 import { Code, Stack } from '@chakra-ui/react'
 import React from 'react'
 
 const About = () => {
   return (
     <div>  
-        <Code>Hello world</Code> 
-        <Stack direction='row'>
-            <Code children='console.log(welcome)' />
-            <Code colorScheme='red' children="var chakra = 'awesome!'" />
-            <Code colorScheme='yellow' children='npm install chakra' />
-        </Stack>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut non suscipit natus, autem a atque iure nulla reiciendis, error eum illum eaque ea porro doloremque exercitationem, 
+            vero praesentium delectus. Ad!
+        </p>
+        <Link href='/' color='green.400' _hover={{ color: 'green.500' }}>
+            Home page
+        </Link>
     </div>
   )
 }
